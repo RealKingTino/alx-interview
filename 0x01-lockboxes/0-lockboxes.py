9528922eed8c07e762b6b695b3551c9f9c0e5bbe
RealKingtino <tmowizzy@gmail.com>
2024-01-13 01:32:35 -0800

    pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 ''' LockBoxes '''
 
+
 def canUnlockAll(boxes):
     ''' Initialize a set to keep track of visited boxes '''
     visited = set()
     stack = boxes[0]
     visited.add(0)
-    
+
     while stack:
         key = stack.pop()
-        
+
         if key < len(boxes) and key not in visited:
             visited.add(key)
             stack.extend(boxes[key])
-    
+
     return len(visited) == len(boxes)
